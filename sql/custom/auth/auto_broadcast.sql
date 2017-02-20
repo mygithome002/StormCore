b12d499b1e19c9fd5e7736d980a6281ac06856a9
Exitare <business.exitare@icloud.com>
2017-02-20 17:38:13 +0100

    Add new logs

---
+++ sql/custom/auth/auto_broadcast.sql
@@ -0,0 +1,3 @@
+DELETE FROM `autobroadcast` where realmid = 3 AND `id` BETWEEN 100 AND 150;
+
+INSERT INTO `autobroadcast` (`realmid`, `id`, `weight`, `text`) VALUES ('3', '100', '2', 'For all workaround type .ex. This will display all workaround commands!');
