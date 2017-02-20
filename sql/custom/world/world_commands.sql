daf10ef93480942a2802a5083856b900e2f233bb
Exitare <business.exitare@icloud.com>
2017-02-20 17:26:57 +0100

    add missing sql

--- sql/custom/world/world_commands.sql
+++ sql/custom/world/world_commands.sql
@@ -13,3 +13,27 @@ INSERT INTO `command` (`name`, `permission`, `help`) VALUES ('ex skipwod', 195, 
 
 DELETE FROM command WHERE `name`='ex skiplegion';
 INSERT INTO `command` (`name`, `permission`, `help`) VALUES ('ex skiplegion', 195, ''); 
+
+DELETE FROM command WHERE `name`='ex startwod';
+INSERT INTO `command` (`name`, `permission`, `help`) VALUES ('ex startwod', 195, ''); 
+
+
+/* Report Commands*/
+
+
+DELETE FROM command WHERE `name`='report';
+INSERT INTO `command` (`name`, `permission`, `help`) VALUES ('report', 195, '');   
+
+DELETE FROM command WHERE `name`='report quest';
+INSERT INTO `command` (`name`, `permission`, `help`) VALUES ('report quest', 195, '');   
+
+DELETE FROM command WHERE `name`='report deactivate';
+INSERT INTO `command` (`name`, `permission`, `help`) VALUES ('report deactivate', 193, '');  
+
+DELETE FROM command WHERE `name`='report activate';
+INSERT INTO `command` (`name`, `permission`, `help`) VALUES ('report activate', 193, '');  
+
+
+/* Custom Xp Rate */
+DELETE FROM command WHERE `name`='xprate';
+INSERT INTO `command` (`name`, `permission`, `help`) VALUES ('xprate', 195, '');  
