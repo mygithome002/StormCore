7f91b47bf733d22c0477e68e775f4954fa6e1150
Exitare <business.exitare@icloud.com>
2017-02-20 17:41:45 +0100

    Add missing auto broadcasts

--- sql/custom/auth/auto_broadcast.sql
+++ sql/custom/auth/auto_broadcast.sql
@@ -1,3 +1,5 @@
 DELETE FROM `autobroadcast` where realmid = 3 AND `id` BETWEEN 100 AND 150;
 
 INSERT INTO `autobroadcast` (`realmid`, `id`, `weight`, `text`) VALUES ('3', '100', '2', 'For all workaround type .ex. This will display all workaround commands!');
+INSERT INTO `autobroadcast` (`realmid`, `id`, `weight`, `text`) VALUES ('3', '101', '2', 'To report a buggy quest just type .report quest [Shift-Left Click on Quest], Your errormessage ');
+INSERT INTO `autobroadcast` (`realmid`, `id`, `weight`, `text`) VALUES ('3', '102', '2', 'To change your XP Rate just type .xprate. ');
